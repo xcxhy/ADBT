6780699427af12e838691f2bfb49d98329b5c7a6
xcxhy <1641173099@qq.com>
2023-02-28 21:17:58 +0800

    commit

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+import sys
+from sacred import Experiment
+
+ex = Experiment("Blip2_Retrieval")
+
+@ex.config
+def config():
+    model_name="blip2_t5"
+    model_type="pretrain_flant5xl"
+    embedd_name="blip_feature_extractor"
+    embedd_type="base"
+    mode="process"
+    product_embedd_path="/data/xcxhy/LAVIS-main/examples/retrivael/data/product_embed.pt"
+    product_text_dir="/data/xcxhy/LAVIS-main/examples/retrivael/data/captions.txt"
+    product_img_dir="/data/xcxhy/LAVIS-main/examples/retrivael/data/image_url.txt"
+
