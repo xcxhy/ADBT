1fd57e33aae42562cc04cde9c00d2649c442b7e3
xcxhy <1641173099@qq.com>
2023-03-02 17:11:58 +0800

    add

---
+++ Text_only/text_qa/test_chat.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import os
+import openai
+
+openai.api_key = 
+
+
+sys_content = input("您希望我扮演一个什么角色: ")
+con_messages = [{"role": "system", "content": sys_content},]
+while True:
+    system = {"role": "system", "content": sys_content}
+    question = input("请输入你的问题: ")
+    new_quesiton = {"role": "user", "content": question}
+    con_messages.append(new_quesiton)
+    result = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=con_messages
+    )
+    sentences = result["choices"]
+    answer = sentences[0]["message"]
+    answer_role = answer['role']
+    answer_content = answer["content"]
+    print("Answer: " + answer_content)
+    con_messages.append({"role": answer_role, "content": answer_content})
