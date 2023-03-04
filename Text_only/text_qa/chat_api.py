15e83110eec9561853561d1a89f1cf6d4b6a488e
xcxhy <1641173099@qq.com>
2023-03-04 22:06:42 +0800

    add

--- Text_only/text_qa/chat_api.py
+++ Text_only/text_qa/chat_api.py
@@ -4,16 +4,22 @@ from loguru import logger
 import streamlit as st
 from sacred import Experiment
 
+st.title("Hello")
 openai.api_key = "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"
 
 logger.add('chat_log.log',retention="20 days")
 
-sys_content = input("您希望我扮演一个什么角色: ")
+sys_content = st.text_input(label="你希望我扮演什么角色: ")
+# sys_content = input("您希望我扮演一个什么角色: ")
 logger.info("system_role: " + sys_content)
 con_messages = [{"role": "system", "content": sys_content},]
-while True:
-    system = {"role": "system", "content": sys_content}
-    question = input("请输入你的问题: ")
+i = 0
+# 
+
+system = {"role": "system", "content": sys_content}
+question = st.text_input(label="请输入你的问题: ")
+if st.button("Enter"):
+        # question = input("请输入你的问题: ")question = st.text_input(label="请输入你的问题: ")
     logger.info("user_question: " + question)
     new_quesiton = {"role": "user", "content": question}
     con_messages.append(new_quesiton)
@@ -25,7 +31,6 @@ while True:
     answer = sentences[0]["message"]
     answer_role = answer['role']
     answer_content = answer["content"]
+    st.write("answer: ", answer_content)
     logger.info("answer: " + answer_content)
     con_messages.append({"role": answer_role, "content": answer_content})
-
-
