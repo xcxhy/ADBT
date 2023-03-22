c7a4795953b9642c347a464f0772a24539d00acc
xcxhy <1641173099@qq.com>
2023-03-22 21:12:52 +0800

    add util

--- util.py
+++ util.py
@@ -20,3 +20,18 @@ def write_pkl(path, data):
     except:
         print("Write pkl Error!")
         return
+    
+def read_list_txt(path):
+    with open(path, "r") as f:
+        data = f.readlines()
+    for i in range(len(data)):
+        data[i] = data[i].strip("\n")
+    print("Read {} List End!".format(path))
+    return data
+
+def write_list_txt(path, data):
+    with open(path, 'w') as f:
+        for value in data:
+            f.write(value + '\n')
+    print("Write {} List End!".format(path))
+    return 
