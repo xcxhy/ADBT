bc34c63a6f9406897a93192a57a8ab2369a6bd6a
xcxhy <1641173099@qq.com>
2023-03-23 14:00:17 +0800

    add kmeans

--- Text_only/text_clustering/kmeans.py
+++ Text_only/text_clustering/kmeans.py
@@ -6,4 +6,12 @@ from pathlib import *
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.cluster import KMeans
 
-cateid_to_catename_dict = read_pkl()
+cateid_to_catename_dict = read_pkl('')
+cates = [cateid_to_catename_dict[key][1] for key in cateid_to_catename_dict.keys()]
+
+count_vect = CountVectorizer()
+
+bow = count_vect.fit_transform(cates)
+
+terms = count_vect.get_feature_names_out()
+
