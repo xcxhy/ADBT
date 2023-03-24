cf9141d70a677cc183806ba5149e00b2b2d1829b
xcxhy <1641173099@qq.com>
2023-03-24 17:08:27 +0800

    add

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ ex = Experiment("Blip2_Retrieval")
 def config():
     model_name="blip2_t5"
     model_type="pretrain_flant5xl"
-    embedd_name="blip_feature_extractor"
-    embedd_type="base"
+    embedd_name="blip2_feature_extractor"
+    embedd_type="pretrain"
     mode="process"
     product_embedd_path="/data/xcxhy/LAVIS-main/examples/retrivael/data/product_embed.pt"
     product_text_dir="/data/xcxhy/LAVIS-main/examples/retrivael/data/captions.txt"
