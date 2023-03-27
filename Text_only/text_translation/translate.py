b9c8c5ebb5f9c81cad3c8678cb39922c3d1cec4c
xcxhy <1641173099@qq.com>
2023-03-27 14:24:28 +0800

    add translate

---
+++ Text_only/text_translation/translate.py
@@ -0,0 +1,22 @@
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+class Translation(object):
+    def __init__(self, name):
+        self.tokenizer = AutoTokenizer.from_pretrained(name)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(name)
+    
+    def tokenize(self, text):
+        token_text = self.tokenizer(text, return_tensors="pt")
+        return token_text
+    
+    def translate_en(self, text):
+        token = self.tokenize(text)
+        translated_token = self.model.generate(**token, forced_bos_token_id=self.tokenizer.lang_code_to_id['eng_Latn'])
+        en_text = self.tokenizer.batch_decode(translated_token, skip_special_tokens=True)[0]
+        return en_text
+    def translate_zh(self, text):
+        token = self.tokenize(text)
+        translated_token = self.model.generate(**token, forced_bos_token_id=self.tokenizer.lang_code_to_id['zho_Hans'])
+        zh_text = self.tokenizer.batch_decode(translated_token, skip_special_tokens=True)[0]
+        return zh_text
+
