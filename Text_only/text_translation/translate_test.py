01d7934b753102d0c7719e170503f5c481fd8400
xcxhy <1641173099@qq.com>
2023-03-30 20:12:37 +0800

    add trans

--- Text_only/text_translation/translate_test.py
+++ Text_only/text_translation/translate_test.py
@@ -1,5 +1,10 @@
 import os
 from translate import Translation
 
-trans = Translation("facebook/nllb-200-1.3B")
+trans = Translation("facebook/nllb-200-distilled-600M")
+
+while True:
+    text = input("please enter your sentence: ")
+    trans_text = trans.translate_zh(text)
+    print(trans_text)
 
