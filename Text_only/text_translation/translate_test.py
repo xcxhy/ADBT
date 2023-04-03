b04099c1108063d73792ef53f5368308f8616d23
xcxhy <1641173099@qq.com>
2023-04-03 17:56:52 +0800

    add

--- Text_only/text_translation/translate_test.py
+++ Text_only/text_translation/translate_test.py
@@ -1,10 +1,17 @@
 import os
+import argparse
 from translate import Translation
 
-trans = Translation("facebook/nllb-200-1.3B")
+def trans(name):
+    trans = Translation(name)
 
-while True:
-    text = input("please enter your sentence: ")
-    trans_text = trans.translate_zh(text)
-    print(trans_text)
+    while True:
+        text = input("please enter your sentence: ")
+        trans_text = trans.translate_zh(text)
+        print(trans_text)
 
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--trans_name', type=str, default="facebook/nllb-200-1.3B")
+    args = parser.parse_args()
+    trans(args.trans_name)
